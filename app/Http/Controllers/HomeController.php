afa4dced8372bf9fc5fd59fb771ad96f9882fec9
Silvino Aguiar <2121100431@soy.utj.edu.mx>
2023-10-31 01:59:37 -0600

    Vista Principal (Home Cards)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,20 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Video;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
@@ -23,6 +13,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $videos = Video::where('status', '=', 1)
+            ->join('users', 'users.id', '=', 'videos.user_id')
+            ->select('videos.*', 'users.name', 'users.id as id_user', 'users.email')
+            ->get();
+        return view('home')->with('videos', $videos);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,8 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }} " />
+
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
